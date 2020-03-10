3fdc99f48baf7a748a0e37a42ee15335efeacdef
Kost911 <Powerofthekee@mail.ru>
2020-03-10 21:32:53 +0300

    Task_1 commit

---
+++ CSharp.Practice/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short number1 = 10;
+            object num = number1;
+
+            sbyte result = (sbyte)(short)num;
+            Console.WriteLine(result);
+        }
+    }
+}

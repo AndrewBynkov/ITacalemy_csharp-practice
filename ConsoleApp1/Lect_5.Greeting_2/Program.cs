45b0786329c513d7498761923b1444f7d0a727aa
Admin <Admin@DESKTOP-A2TKCRO>
2020-03-18 21:36:31 +0300

    completion of the task

---
+++ ConsoleApp1/Lect_5.Greeting_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lect_5.Greeting_2
+{
+    class Program
+    {
+        private static char symb;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you name");
+            string myName = Console.ReadLine();
+            int i = 0;
+
+            foreach (char symb in myName) 
+            {
+                if (symb == 'a')
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine("The number of letters 'a' in this word is :" + i);
+        }
+    }
+}
